fc65de9d16c2609815da5467825291cc8f6ee2e7
Andrew Robinson <SirArkimedes@gmail.com>
2015-01-04 21:54:39 -0600

    Add a simple beginning UI

---
+++ Source/Menu.m
@@ -0,0 +1,18 @@
+//
+//  Menu.m
+//  Delivery
+//
+//  Created by Andrew Robinson on 1/4/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "Menu.h"
+
+@implementation Menu
+
+- (void)play {
+    CCScene *gameplayScene = [CCBReader loadAsScene:@"MainScene"];
+    [[CCDirector sharedDirector] replaceScene:gameplayScene];
+}
+
+@end
