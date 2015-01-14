36660a5be6fc249c59a03d7eaedb98759ec272fd
Andrew Robinson <SirArkimedes@gmail.com>
2015-01-14 11:48:06 -0600

    Reorganize files

---
+++ Source/Stats.h
@@ -0,0 +1,13 @@
+//
+//  Stats.h
+//  Delivery
+//
+//  Created by Andrew Robinson on 1/14/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Stats : CCNode
+
+@end
---
+++ Source/Stats.m
@@ -0,0 +1,13 @@
+//
+//  Stats.m
+//  Delivery
+//
+//  Created by Andrew Robinson on 1/14/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "Stats.h"
+
+@implementation Stats
+
+@end
