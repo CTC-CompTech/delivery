e32ffda6856ed815fdbbc8ba182bfbc2a4d7c2f6
Andrew Robinson <SirArkimedes@gmail.com>
2015-02-09 22:17:43 -0600

    Car Menu organzing

--- Source/CarMenu.m
+++ Source/CarMenu.m
@@ -9,13 +9,43 @@
 #import "CarMenu.h"
 #import "Stats.h"
 
+static const CGFloat scrollSpeed = 210.f;
+
 @implementation CarMenu {
     CCButton *_backCar;
+    
+    CCPhysicsNode *_physicsNode;
+    CCNode *_ground1;
+    CCNode *_ground2;
+    
+    NSArray *_grounds;
     
     // TEMP
     CCButton *_addBtn;
     CCButton *_clear;
     // TEMP
+}
+
+- (void)didLoadFromCCB {
+     _grounds = @[_ground1, _ground2];
+}
+
+- (void)update:(CCTime)delta {
+    
+    _physicsNode.position = ccp(_physicsNode.position.x - (scrollSpeed *delta), _physicsNode.position.y);
+    
+    // loop the ground
+    for (CCNode *ground in _grounds) {
+        // get the world position of the ground
+        CGPoint groundWorldPosition = [_physicsNode convertToWorldSpace:ground.position];
+        // get the screen position of the ground
+        CGPoint groundScreenPosition = [self convertToNodeSpace:groundWorldPosition];
+        // if the left corner is one complete width off the screen, move it to the right
+        if (groundScreenPosition.x <= (-1 * ground.contentSize.width)) {
+            ground.position = ccp(ground.position.x + 2 * ground.contentSize.width - 1, ground.position.y);
+//            NSLog(@"%@", NSStringFromCGPoint(ground.position));
+        }
+    }
 }
 
 - (void)clear {
