adda5bf4dd9166674e3ec39b923c8a3673e18ee3
gajbooks <8304781549@carthagetigermail.com>
2015-03-03 08:45:51 -0800

    Sports Car Update
    Updated the sports car to use the updated ability system.

--- Source/sportsCar.m
+++ Source/sportsCar.m
@@ -8,6 +8,22 @@
 
 #import "sportsCar.h"
 
+#define JEEP_ABILITY_DURATION 5.0f
+#define JEEP_ABILITY_COOLDOWN 5.0f
+
+@interface sportsCar ()
+
+@property (nonatomic) double preAbilitySpeed;
+
+@property (nonatomic, weak) CCParticleSystem* particleEffect;
+
+@property (nonatomic, weak) CCNode* abilityButton;
+
+@property (nonatomic, weak) CCLabelTTF* countdownTimer;
+
+@end
+
+
 @implementation sportsCar
 
 #pragma mark - init
@@ -26,6 +42,78 @@
     }
     else
         return nil;
+}
+
+
+-(void)setupVehicle{
+    [self.parentVehicle.scene addChild:[CCBReader load:@"Jeep Overlay" owner:self]];
+    self.particleEffect.visible = false;
+    [self.particleEffect removeFromParentAndCleanup:NO];
+    [self.parentVehicle addChild:self.particleEffect];
+}
+
+-(void)useAbility{
+    if (self.canUseAbility){
+        self.preAbilitySpeed = self.vehicleSpeed;
+        self.abilityCooldown = JEEP_ABILITY_COOLDOWN;
+        self.abilityTimeout = JEEP_ABILITY_DURATION;
+        self.vehicleSpeed = self.preAbilitySpeed*3;
+        [self.particleEffect resetSystem];
+        self.particleEffect.visible = true;
+        self.parentVehicle.physicsBody.collisionType = @"ability";
+        
+        self.canUseAbility = false;
+    }
+}
+
+-(void)abilityUpdate:(CCTime)delta{
+    if (!self.canUseAbility){
+        
+        if (self.abilityTimeout == JEEP_ABILITY_DURATION){ // Do when the ability first starts
+            self.abilityButton.visible = false;
+        }
+        
+        if ((self.abilityTimeout >= 1.5) && ((self.abilityTimeout - delta) < 1.5)){// Do when 1 seconds are remaining on the ability
+            self.vehicleSpeed = self.preAbilitySpeed;
+            [self.particleEffect stopSystem];
+        }
+        
+        if (self.abilityTimeout >= 0){ // Do while the ability is running
+            self.abilityTimeout -= delta;
+        }
+        
+        
+        else if (self.abilityCooldown >= 0){ // Do while the ability is cooling down
+            
+            if ((self.abilityTimeout <= 0) && (self.abilityCooldown == JEEP_ABILITY_COOLDOWN)){ // Do when the ability first cools down
+                self.countdownTimer.visible = true;
+                self.countdownTimer.string = [NSString stringWithFormat:@"%i", (int)self.abilityCooldown];
+                self.particleEffect.visible = false;
+                self.parentVehicle.physicsBody.collisionType = @"hero";
+            }
+            self.countdownTimer.string = [NSString stringWithFormat:@"%i", (int)self.abilityCooldown];
+            self.abilityCooldown -= delta;
+        }
+        
+        else { // Do on ability reset
+            self.countdownTimer.visible = false;
+            self.abilityButton.visible = true;
+            self.canUseAbility = true;
+        }
+        
+        
+    }}
+
+-(void)onPause{
+    [super onPause];
+    [self.abilityOverlay setVisible:false];
+    self.particleEffect.paused = true;
+}
+
+-(void)onResume{
+    [super onResume];
+    [self.abilityOverlay setVisible:true];
+    self.particleEffect.paused = false;
 }
 
 @end
