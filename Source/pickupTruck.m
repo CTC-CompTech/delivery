cdda186767db9b69df3afe41b25d6832b4e6af34
gajbooks <8304781549@carthagetigermail.com>
2015-03-09 07:23:55 -0700

    Convert Pickup truck to slo-mo ability
    Converted the pickup truck to use the new system and have a slo-mo
    ability.

--- Source/pickupTruck.m
+++ Source/pickupTruck.m
@@ -8,6 +8,9 @@
 
 #import "pickupTruck.h"
 
+#define PICKUP_TRUCK_ABILITY_DURATION 5.0f
+#define PICKUP_TRUCK_ABILITY_COOLDOWN 5.0f
+
 @interface pickupTruck ()
 
 @property (nonatomic) double preAbilitySpeed;
@@ -37,5 +40,63 @@
     else
         return nil;
 }
+
+-(void)setupVehicle{
+    [self.parentVehicle.scene addChild:[CCBReader load:@"Pickup Truck Overlay" owner:self]];
+}
+
+-(void)useAbility{
+    if (self.canUseAbility){
+        self.preAbilitySpeed = self.vehicleSpeed;
+        self.abilityCooldown = PICKUP_TRUCK_ABILITY_COOLDOWN;
+        self.abilityTimeout = PICKUP_TRUCK_ABILITY_DURATION;
+        self.vehicleSpeed = self.preAbilitySpeed/1.5;
+        self.canUseAbility = false;
+    }
+}
+
+-(void)abilityUpdate:(CCTime)delta{
+    if (!self.canUseAbility){
+        
+        if (self.abilityTimeout == PICKUP_TRUCK_ABILITY_DURATION){ // Do when the ability first starts
+            self.abilityButton.visible = false;
+        }
+        
+        if (self.abilityTimeout >= 0){ // Do while the ability is running
+            self.abilityTimeout -= delta;
+        }
+        
+        
+        else if (self.abilityCooldown >= 0){ // Do while the ability is cooling down
+            
+            if ((self.abilityTimeout <= 0) && (self.abilityCooldown == PICKUP_TRUCK_ABILITY_COOLDOWN)){ // Do when the ability first cools down
+                self.countdownTimer.visible = true;
+                self.countdownTimer.string = [NSString stringWithFormat:@"%i", (int)self.abilityCooldown];
+                self.vehicleSpeed = self.preAbilitySpeed;
+            }
+            self.countdownTimer.string = [NSString stringWithFormat:@"%i", (int)self.abilityCooldown];
+            self.abilityCooldown -= delta;
+        }
+        
+        else { // Do on ability reset
+            self.countdownTimer.visible = false;
+            self.abilityButton.visible = true;
+            self.canUseAbility = true;
+        }
+        
+        
+    }}
+
+-(void)onPause{
+    [super onPause];
+    [self.abilityOverlay setVisible:false];
+}
+
+-(void)onResume{
+    [super onResume];
+    [self.abilityOverlay setVisible:true];
+}
+
+
 
 @end
